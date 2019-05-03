6a6bbd50c1deadae6b8ca82c4f7b3fb85f2d76e7
Shivam Tayal <shitaya1@in.ibm.com>
2019-05-03 16:04:08 +0530

    Adding PPT of Project

--- Service/src/test/java/com/ibm/diypopups/DiypopupsApplicationTests.java
+++ Service/src/test/java/com/ibm/diypopups/DiypopupsApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class FixmyshipApplicationTests {
+public class DiypopupsApplicationTests {
 
 	@Test
 	public void contextLoads() {
